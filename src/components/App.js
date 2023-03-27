9d1963ba3ffde3ceab2ede07a5629b3d86bd8e23
Meridith Lawn <meridithlawn2@gmail.com>
2023-03-27 17:04:58 -0600

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import About from "./About";
 
 function NavBar() {
   return (
@@ -16,14 +17,19 @@ function Home() {
   );
 }
 
+
+
 {/* write an <About> component here */}
 
 
 function App() {
+  let name = "Meridith"
+  console.log(`${name} loves REACT!`)
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
+      <Home />
+      <About />
       {/* add your <About> component here */}
     </div>
   );
